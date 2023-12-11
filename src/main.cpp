81cccd49dbdc5c236c86254119e2a5daebe77e2b
Mareks Brucis <mareks.brucis@gmail.com>
2023-12-11 19:39:33 +0200

    Add webserver

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,14 @@
-
-#include "FastLED.h"
+#include <WiFi.h>
 #include "pitches.h"
-#define NUM_LEDS 1
+#define NUM_LEDS 3
 
 // Data pin that led data will be written out over
-#define DATA_PIN 6
+#define LED_DATA_PIN 6
 #define BUZZER_PIN 2
-// Clock pin only needed for SPI based chipsets when not using hardware SPI
-//#define CLOCK_PIN 8
+#define DETONATE_PIN 12
+// Replace with your network credentials
+const char* ssid = "HspPhone";
+const char* password = "eprue123";
 int melody[] = {
   REST, REST, REST, NOTE_DS4, 
   NOTE_E4, REST, NOTE_FS4, NOTE_G4, REST, NOTE_DS4,
@@ -28,7 +29,6 @@ int melody[] = {
   NOTE_AS4, NOTE_A4, NOTE_AS4, NOTE_A4, NOTE_AS4, NOTE_A4, NOTE_AS4, NOTE_A4,   
   NOTE_G4, NOTE_E4, NOTE_D4, NOTE_E4, NOTE_E4, NOTE_E4
 };
-
 int durations[] = {
   2, 4, 8, 8, 
   4, 8, 8, 4, 8, 8,
@@ -49,35 +49,122 @@ int durations[] = {
   16, 8, 16, 8, 16, 8, 16, 8,   
   16, 16, 16, 16, 16, 2
 };
-// This is an array of leds.  One item for each led in your strip.
-CRGB leds[NUM_LEDS];
+// Set web server port number to 80
+WiFiServer server(80);
+
+// Variable to store the HTTP request
+String header;
+
+// Current time
+unsigned long currentTime = millis();
+// Previous time
+unsigned long previousTime = 0; 
+// Define timeout time in milliseconds (example: 2000ms = 2s)
+const long timeoutTime = 2000;
 
 void setup() {
-    Serial.begin(921600);
-	// sanity check delay - allows reprogramming if accidently blowing power w/leds
-   	delay(2000);
-    FastLED.addLeds<SM16703, DATA_PIN, BRG>(leds, NUM_LEDS);
+  Serial.begin(921600);
+  pinMode(BUZZER_PIN,OUTPUT);
+  pinMode(DETONATE_PIN,OUTPUT);
+
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  // Print local IP address and start web server
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
 }
+bool on=false;
+String onState="off";
+void loop(){
+  WiFiClient client = server.available();   // Listen for incoming clients
 
-// your leds.
-void loop() {
-   int size = sizeof(durations) / sizeof(int); 
-	 for (int note = 0; note < size; note++) { 
-	   //to calculate the note duration, take one second divided by the note type. 
-	   //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc. 
-	   int duration = 1000 / durations[note]; 
-     leds[0] = CRGB::Red;
-      FastLED.show();
-	   tone(BUZZER_PIN, melody[note], duration); 
-     leds[0] = CRGB::Blue;
-      FastLED.show();
-	   //to distinguish the notes, set a minimum time between them. 
-	   //the note's duration + 30% seems to work well: 
-	   int pauseBetweenNotes = duration * 1.30; 
-	   delay(pauseBetweenNotes); 
-	   //stop the tone playing: 
-	   noTone(BUZZER_PIN); 
-	 } 
+  if (client) {                             // If a new client connects,
+    currentTime = millis();
+    previousTime = currentTime;
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected() && currentTime - previousTime <= timeoutTime) {  // loop while the client's connected
+      currentTime = millis();
+
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            
+            // turns the GPIOs on and off
+            if (header.indexOf("GET /detonate/on") >= 0) {
+              Serial.println("detonate on");
+              digitalWrite(DETONATE_PIN,HIGH);//boom
+              onState = "on";
+              on=true;
+            } else if (header.indexOf("GET /detonate/off") >= 0) {
+              Serial.println("detonate off");
+              digitalWrite(DETONATE_PIN,LOW);//no boom
+              onState = "off";
+              on=false;
+            } 
+            
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            // CSS to style the on/off buttons 
+            // Feel free to change the background-color and font-size attributes to fit your preferences
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".button2 {background-color: #555555;}</style></head>");
+            
+            // Web Page Heading
+            client.println("<body><h1>ESP32 Web Server</h1>");
+            
+            // Display current state, and ON/OFF buttons for detonation
+            client.println("<p>Detonate - State " + onState + "</p>");
+            // If the onState is off, it displays the ON button       
+            if (onState=="off") {
+              client.println("<p><a href=\"/detonate/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/detonate/off\"><button class=\"button button2\">OFF</button></a></p>");
+            } 
+               
+            client.println("</body></html>");
+            
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
 }
-
-
